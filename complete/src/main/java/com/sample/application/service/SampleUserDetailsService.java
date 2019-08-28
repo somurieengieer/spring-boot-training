2ec52346da2e0a7c675ec635d231dd3148409127
somurie06 <somurie06@gmail.com>
2019-08-28 21:32:31 +0900

    認可設定を追加

--- complete/src/main/java/com/sample/application/service/SampleUserDetailsService.java
+++ complete/src/main/java/com/sample/application/service/SampleUserDetailsService.java
@@ -1,13 +1,16 @@
 package com.sample.application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SampleUserDetailsService implements UserDetailsService {
@@ -17,10 +20,14 @@ public class SampleUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        if (username.equals("admin")) authorities.add(new SimpleGrantedAuthority("ADMIN"));
 
-        User.UserBuilder builder = User.withUsername(username)
+        User.UserBuilder builder =
+                User.withUsername(username)
                 .password(passwordEncoder.encode("password"))
-                .authorities(AuthorityUtils.createAuthorityList("ADMIN_USER"));
+                .authorities(authorities);
         // authoritiesは何かしら設定しないとログインできないので注意
 
         return builder.build();
